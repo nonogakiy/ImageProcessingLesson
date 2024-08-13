06c8db03f85d2573aed252ee324e45ba1fb14126
Youichi NONOGAKI <nonogakiy@citizen.co.jp>
2024-08-13 18:04:04 +0900

    トラックバーでサイズ変更

--- src/Lesson05_Filter.py
+++ src/Lesson05_Filter.py
@@ -1,12 +1,26 @@
 import cv2
 
+gaussSize = 0.0
+
+def onValueChanged(pos):
+    global gaussSize
+    gaussSize = pos / 255 * 5
+
 empire_photo = cv2.imread('./data/empire.jpg')
-cv2.imshow('FilterTest', empire_photo)
 
-cv2.waitKey(0)
-
-empire_photo = cv2.GaussianBlur(empire_photo, (5,5), sigmaX = 10.0, sigmaY = 10.0, borderType = cv2.BORDER_DEFAULT)
 cv2.imshow('FilterTest', empire_photo)
-cv2.waitKey(0)
+cv2.createTrackbar('Size', 'FilterTest', 0, 255, onValueChanged)
+
+while True:
+    if gaussSize == 0:
+        disp = empire_photo
+    else:
+        disp = cv2.GaussianBlur(empire_photo, (11,11), gaussSize, borderType = cv2.BORDER_DEFAULT)
+
+    cv2.imshow('FilterTest', disp)
+    key = cv2.waitKey(10)
+
+    if key == ord('q'):
+        break
 
 cv2.destroyAllWindows()
