84221e074422510229595ae4028ded83c69963ff
Youichi NONOGAKI <nonogakiy@citizen.co.jp>
2024-08-13 18:04:43 +0900

    GaussBlur_TrackBar

--- src/Lesson05_Filter.py
+++ src/Lesson05_Filter.py
@@ -1,12 +1,24 @@
 import cv2
 
+gaussSize = 0.0
+
+def onValueChanged(pos):
+    global gaussSize
+    gaussSize = pos / 255 * 5
+
 empire_photo = cv2.imread('./data/empire.jpg')
 cv2.imshow('FilterTest', empire_photo)
+cv2.createTrackbar('Size', 'FilterTest', 0, 255, onValueChanged)
 
-cv2.waitKey(0)
+while True:
+    if gaussSize == 0:
+        disp = empire_photo
+    else:
+        disp = cv2.GaussianBlur(empire_photo, (11,11), gaussSize, borderType = cv2.BORDER_DEFAULT)
+    cv2.imshow('FilterTest', disp)
+    key = cv2.waitKey(10)
 
-empire_photo = cv2.GaussianBlur(empire_photo, (5,5), sigmaX = 10.0, sigmaY = 10.0, borderType = cv2.BORDER_DEFAULT)
-cv2.imshow('FilterTest', empire_photo)
-cv2.waitKey(0)
+    if key == ord('q'):
+        break
 
 cv2.destroyAllWindows()
