c2f222518d5bee7b7b3c415c02f1e05c5d91f9ab
Youichi NONOGAKI <nonogakiy@citizen.co.jp>
2024-08-14 10:48:52 +0900

    blur_bugFix

--- src/Lesson05_Filter.py
+++ src/Lesson05_Filter.py
@@ -1,32 +1,33 @@
 import cv2
+import math
 
-blurSize = 0.0
-medianSize = 0.0
+blurSize : int = 0
+medianSize : int = 0
 gaussSize = 0.0
 
 def onBlurSizeChanged(pos):
     global blurSize
     global medianSize
     global gaussSize
-    blurSize = pos / 255 * 5
-    medianSize = 0.0
+    blurSize = math.floor(pos / 8)
+    medianSize = 0
     gaussSize = 0.0
 
 def onMedianSizeChanged(pos):
     global blurSize
     global medianSize
     global gaussSize
-    blurSize = 0.0
-    medianSize = pos / 255 * 5
+    blurSize = 0
+    medianSize = math.floor(pos / 8 ) * 2 + 1
     gaussSize = 0.0
 
 def onGaussSizeChanged(pos):
     global blurSize
     global medianSize
     global gaussSize
-    blurSize = 0.0
-    medianSize = 0.0
-    gaussSize = pos / 255 * 5
+    blurSize = 0
+    medianSize = 0
+    gaussSize = pos / 255 * 20
 
 empire_photo = cv2.imread('./data/empire.jpg')
 
@@ -36,18 +37,18 @@ cv2.createTrackbar('Median', 'FilterTest', 0, 255, onMedianSizeChanged)
 cv2.createTrackbar('Gauss', 'FilterTest', 0, 255, onGaussSizeChanged)
 
 while True:
-    if blurSize != 0.0:
-        disp = cv2.blur(empire_photo, (blurSize,blurSize), (-1,-1), cv2.BORDER_DEFAULT )
-        cv2.setTrackbarPos('Median', 'FilterTest', medianSize)
-        cv2.setTrackbarPos('Gauss', 'FilterTest', gaussSize)
-    elif medianSize != 0.0:
-        disp = cv2.medianBlur(empire_photo, (medianSize,medianSize))
-        cv2.setTrackbarPos('Blur', 'FilterTest', blurSize)
-        cv2.setTrackbarPos('Gauss', 'FilterTest', gaussSize)
+    if blurSize != 0:
+        disp = cv2.blur(empire_photo, (int(blurSize), int(blurSize)))
+        cv2.setTrackbarPos('Median', 'FilterTest', 0)
+        cv2.setTrackbarPos('Gauss', 'FilterTest', 0)
+    elif medianSize != 0:
+        disp = cv2.medianBlur(empire_photo, int(medianSize))
+        cv2.setTrackbarPos('Blur', 'FilterTest', 0)
+        cv2.setTrackbarPos('Gauss', 'FilterTest', 0)
     elif gaussSize != 0.0:
-        disp = cv2.GaussianBlur(empire_photo, (11,11), gaussSize, borderType = cv2.BORDER_DEFAULT)
-        cv2.setTrackbarPos('Blur', 'FilterTest', blurSize)
-        cv2.setTrackbarPos('Median', 'FilterTest', medianSize)
+        disp = cv2.GaussianBlur(empire_photo, (31,31), gaussSize, borderType = cv2.BORDER_DEFAULT)
+        cv2.setTrackbarPos('Blur', 'FilterTest', 0)
+        cv2.setTrackbarPos('Median', 'FilterTest', 0)
     else:
         disp = empire_photo
     
